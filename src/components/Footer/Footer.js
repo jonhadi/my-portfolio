8914588592b446840aff4d4c6be2e0e5c8ad1fb5
jonhadi <hadijonathan@gmail.com>
2020-02-05 21:08:05 -0800

    fixed links and added text

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -12,15 +12,15 @@ class NavBar extends Component {
                     <div className="col-6">
                         <a href="mailto:hadijonathan@gmail.com"> <img id="mail-button" src="/images/mail.png" alt=""></img>Email</a></div>
                     <div className="col-6">
-                        <a href="https://www.linkedin.com/in/jonhadi"> <img id="github-button" src="/images/github.png" alt=""></img></a>
+                        <a href="https://www.github.com/jonhadi"> <img id="github-button" src="/images/github.png" alt=""></img>Github</a>
                     </div>   
                 </div>
                 <div className="row">
                     <div className="col-6">
-                        <a href="https://www.linkedin.com/in/jonhadi"> <img id="linkedin-button" src="/images/linkedin.png" alt=""></img></a>
+                        <a href="https://www.linkedin.com/in/jonhadi"> <img id="linkedin-button" src="/images/linkedin.png" alt="">LinkedIn</img></a>
                     </div>
                     <div className="col-6">
-                        <a href="https://www.instagram.com/jonflamo/"> <img id="instagram-button" src="/images/instagram.png" alt=""></img></a>
+                        <a href="https://www.instagram.com/jonflamo/"> <img id="instagram-button" src="/images/instagram.png" alt=""></img>Instagram</a>
                     </div>
                 </div>
             </div>
