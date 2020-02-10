319878be51df9bc0c16eab60da61e04bc4aa7247
jonhadi <hadijonathan@gmail.com>
2020-02-09 19:01:01 -0800

    fix links

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -8,21 +8,21 @@ class NavBar extends Component {
     return (
         <nav className="navbar navbar-expand-sm">
             <div className="container">
-                <a className="full-width" href="#top"><img id="logo" alt="" src="/images/logo.png"></img></a>
+                <a className="navbar-brand" href="#top"><img id="logo" alt="" src="/images/logo.png"></img></a>
 
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup"
                     aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
 
-            <div className="collapse navbar-collapse justify-content-end" id="navbarNavAltMarkup">
-                <div className="navbar-nav">
-                    <a id="home" className="nav-item nav-link active" href="#top">Home</a>
-                    <a id="learn" className="nav-item nav-link" href="#story-container">Story</a>
-                    <a id="events" className="nav-item nav-link" href="#project-show">Portfolio</a>
-                    <a id="test" className="nav-item nav-link" href="#footer">Contact</a>
+                <div className="collapse navbar-collapse justify-content-end" id="navbarNavAltMarkup">
+                    <div className="navbar-nav">
+                        <a id="home" className="nav-item nav-link" href="#top">Home</a>
+                        <a id="story" className="nav-item nav-link" href="#story-container">Story</a>
+                        <a id="portfolio" className="nav-item nav-link" href="#project-show">Portfolio</a>
+                        <a id="contact" className="nav-item nav-link" href="#footer">Contact</a>
+                    </div>
                 </div>
-            </div>
             </div>
         </nav>
     )
